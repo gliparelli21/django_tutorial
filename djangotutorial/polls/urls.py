6fe0bcc2b9b4fda37660c5faa38176f9938dcf4f
gliparelli21 <20231824@novaims.unl.pt>
2026-02-26 00:39:51 +0000

    Tutorial 2 - Until Make the poll app modifiable

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -1,8 +1,7 @@
+from django.urls import path
+
 from . import views
-from django.contrib import admin
-from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
 ]
